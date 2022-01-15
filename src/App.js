70671f05130209ebea24f8a1147d68ec60c04cb4
sumi005 <salli7635@richmondhillhs.org>
2022-01-15 00:14:57 +0000

    NYSED-lab-changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Navbar from './components/Navbar.js';
 // Be sure to uncomment these components and the data before you use them!
-// import SchoolCard from './components/SchoolCard.js';
-// import data from './nycSchoolEnrollmentData.js';
+import SchoolCard from './components/SchoolCard.js';
+import data from './nycSchoolEnrollmentData.js';
 import './App.css';
 
 function App() {
@@ -11,7 +11,12 @@ function App() {
       <Navbar/>
       <div className="container">
         <div className="row">
-
+            <SchoolCard school = {data[0]}/>
+            <SchoolCard school = {data[1]}/>
+            <SchoolCard school = {data[2]}/>
+            <SchoolCard school = {data[3]}/>
+            <SchoolCard school = {data[4]}/>
+            <SchoolCard school = {data[5]}/>
         </div>
       </div>
     </div>
